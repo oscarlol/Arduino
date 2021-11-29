d8c2b333792a67dddeae050846bbf9eac6c4e169
Oscar Pradas 2021 <oscararrels@gmail.com>
2021-11-29 16:21:39 +0100

    Update ex7.ino

--- Tipus de Dades i Operadors/ex7/ex7.ino
+++ Tipus de Dades i Operadors/ex7/ex7.ino
@@ -7,38 +7,34 @@
  * ******************************************************************/
 //************************** INCLUDE *********************************
 
+
+
 //********** Variables ************************************************************
 int drive_gb = 100;
-long drive_mb, drive_kb, real_drive_mb, real_drive_kb, missing_drive_kb;
+int drive_mb = 0;
 
 //********** Setup ****************************************************************
-void setup()
+void setup()             // run once, when the sketch starts
 {
-Serial.begin(9600);
-Serial.print("Your HD is ");
-Serial.println(drive_gb);
-drive_mb = drive_gb;
-drive_mb = drive_mb * 1024;
-drive_kb = drive_mb * 1024;
-Serial.print("In theory, it can store ");
-Serial.print(drive_mb);
-Serial.print("MB , ");
-Serial.print(drive_kb);
-Serial.println("KB");
-drive_mb = drive_gb;
-real_drive_mb = drive_mb * 1000;
-real_drive_kb = real_drive_mb * 1000;
-Serial.print("But it really only store ");
-Serial.print(real_drive_mb);
-Serial.print("MB , "); 
-Serial.print(real_drive_kb);
-Serial.println("KB");
-missing_drive_kb = drive_kb - real_drive_kb;
-Serial.print("You are missing ");
-Serial.print(missing_drive_kb);
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
+
+  drive_mb = 1024 * drive_gb;
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
 }
 
 //********** Loop *****************************************************************
-void loop()
-{
+
+
+//*************************** LOOP ***********************************
+
+void loop() {
+
+
 }
+//**************************FUNCIONS**********************************
