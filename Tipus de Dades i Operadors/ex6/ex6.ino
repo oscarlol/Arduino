150eb2b7180eb3f374da08f8bbfd1bac5fb91e66
Oscar Pradas 2021 <oscararrels@gmail.com>
2021-11-29 16:03:44 +0100

    Create ex6.ino

---
+++ Tipus de Dades i Operadors/ex6/ex6.ino
@@ -0,0 +1,40 @@
+/*********************************************************************
+ * *                                                                **
+ * *                            Titol:                              **
+ * *                        nom del programa                        **
+ * *                                                                **
+ * *  Nom:Oscar Pradas                             Data:xx/xx/2021  **
+ * ******************************************************************/
+//************************** INCLUDE *********************************
+
+
+
+//********** Variables ************************************************************
+int drive_gb = 5;
+int drive_mb = 0;
+
+//********** Setup ****************************************************************
+void setup()             // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
+
+  drive_mb = 1024 * drive_gb;
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
+}
+
+//********** Loop *****************************************************************
+
+
+//*************************** LOOP ***********************************
+
+void loop() {
+
+
+}
+//**************************FUNCIONS**********************************
